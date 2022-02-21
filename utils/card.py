4560c4a019ba5a80e4ceba716733ae9280d196f1
Len-Fid <yelena.fidrmuc@gmail.com>
2022-02-21 17:04:21 +0100

    adding  __str__() method

--- utils/card.py
+++ utils/card.py
@@ -3,11 +3,14 @@ class Symbol:
     icon =  ["♥","♦","♣","♠"]
     
     def __init__(self, color, icon):
-    self.color = color
-    self.icon = icon
+        self.color = color
+        self.icon = icon
+    
+    def __str__ (self):
+        return f"{self.color} {self.icon}"
     
 class Card(Symbol): # inherits from Symbol
-value = ["A",2", "3","4", 
+    value = ["A","2", "3","4", 
            "5", "6", "7",
            "8", "9", "10",
            "J", "Q","K"]
@@ -15,3 +18,9 @@ value = ["A",2", "3","4",
     def __init__(self, color, icon,value):
         super().__init__(color, icon)
         self.value = value
+
+    def __str__ (self):
+        return f"{self.color} {self.icon} {self.value}"
+
+my_card = Card ("red","♦","Q")
+print (my_card)
