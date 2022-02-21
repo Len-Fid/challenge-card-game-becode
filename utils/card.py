a0962da9e5361ecf1261932df674822b04ab1dfe
Len-Fid <yelena.fidrmuc@gmail.com>
2022-02-21 16:29:49 +0100

    draft version

---
+++ utils/card.py
@@ -0,0 +1,16 @@
+class Symbol:
+    color = ["red","black"]
+    icon =  ["♥","♦","♣","♠"]
+    
+    def __init__(self, color, icon):
+    self.color = color
+    self.icon = icon
+    
+class Card(Symbol): # inherits from Symbol
+value = ["A",2", "3","4", 
+           "5", "6", "7",
+           "8", "9", "10",
+           "J", "Q","K"]
+ 
+    def __init__(self, color, icon,value):
+    self.value = value
